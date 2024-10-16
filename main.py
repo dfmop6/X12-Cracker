3cd35692866d3933ddf5d0ff1e7389f8b53b0730
dfmop6 <ss.elmachehour@gmail.com>
2024-10-16 20:18:13 +0100

    update details list for data elements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pdfplumber
+import re
 
 def int_checker(string_val):
     try:
@@ -10,20 +11,46 @@ def int_checker(string_val):
 def trim_string(string):
   return string.strip()
 
+def clean_string_format(input_string):
+    result, tt = [], []
+    vv = input_string.split(" ")
+    for r in vv:
+        result = []
+        for i, letter in enumerate(r):
+            if i % 2 == 1:
+                continue
+            result.append(letter)
+        tt.append(''.join(result))
+
+    return ' '.join(tt)
+
+def detail_process(data_list):
+    recovred_list = []
+    for i, seg in enumerate(data_list):
+        first_elmnt = seg.split(" ")[0]
+        if first_elmnt in loop__keys:
+            recovred_list.append(clean_string_format(seg))
+        elif int_checker(first_elmnt):
+            recovred_list.append(seg)
+        else:
+            recovred_list.append(data_list[i-1] + " " + data_list[i])
+            recovred_list.remove(data_list[i-1])
+    
+    return recovred_list
+
 source_file = "data/The_Home_Depot_-_EDI_Specifications_and_Examples_-_Suppliers[1] (1).pdf" 
 maintable_config = {
     'headers':["Pos", "Id", "Segment Name", "Req", "Max Use", "Repeat", "Notes", "Usage"]
 }
 maintable_data = {}
-
 head_doors = {
     'ISA': 'Interchange Control Header',
     'GS': 'Functional Group Header',
     'GE': 'Functional Group Trailer',
     'IEA': 'Interchange Control Trailer'
 }
-
 big__titels = ["Heading", "Not Defined", 'Detail', 'tail']
+loop__keys = ["LLOOOOPP", 'LOOP']
 
 with pdfplumber.open(source_file) as pdf:
     # for page in pdf.pages[13]:        
@@ -74,30 +101,32 @@ with pdfplumber.open(source_file) as pdf:
 
 
     # Envelope data processing
-
     fixedTails = ["Must use", "Used"]
     maintable_config['table'] = []
     for k, v in content:
-        for x in v:
-            c_val = x.split(" ")
-            if k in ["Not Defined", "tail"] and not int_checker(head_doors[c_val[0]]):
-                leading = ['None', c_val[0],head_doors[c_val[0]]]
-                beg = c_val[0] + " " + head_doors[c_val[0]]
-                tail = x.split(beg)[1]
-                if "Must use" in tail:
-                    tail = trim_string(tail.split("Must use")[0])
-                    middle_values = tail.split(" ")
-                    middle_values.append("Must use")
-                else:
-                    tail = trim_string(tail.split("Must use")[0])
-                    middle_values = tail.split(" ")
-                    middle_values.append("Used")
+        if k in ["Not Defined", "tail"]:
+            for x in v:
+                c_val = x.split(" ")
+                if not int_checker(head_doors[c_val[0]]):
+                    leading = ['None', c_val[0],head_doors[c_val[0]]]
+                    beg = c_val[0] + " " + head_doors[c_val[0]]
+                    tail = x.split(beg)[1]
+                    if "Must use" in tail:
+                        tail = trim_string(tail.split("Must use")[0])
+                        middle_values = tail.split(" ")
+                        middle_values.append("Must use")
+                    else:
+                        tail = trim_string(tail.split("Must use")[0])
+                        middle_values = tail.split(" ")
+                        middle_values.append("Used")
 
-                leading.extend(middle_values)
-                maintable_config['table'].append(leading)
-            
-            else:
-                ...
+                    leading.extend(middle_values)
+                    maintable_config['table'].append(leading)
+        else:
+            d = detail_process(v)
+            maintable_config['table'].append(d)
+
 
 
     print(maintable_config)
+
